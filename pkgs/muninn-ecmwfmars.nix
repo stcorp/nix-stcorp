b26f2673ca657606d687fc0a1085177fda152889
Sander Niemeijer <sander.niemeijer@stcorp.nl>
2023-07-05 14:31:27 +0200

    Update for muninn-ecmwfmars 1.2

--- pkgs/muninn-ecmwfmars.nix
+++ pkgs/muninn-ecmwfmars.nix
@@ -4,11 +4,11 @@ with builtins;
 
 buildPythonPackage {
   pname = "muninn-ecmwfmars";
-  version = "1.1";
+  version = "1.2";
 
   src = fetchurl {
-    url = "https://github.com/stcorp/muninn-ecmwfmars/archive/1.1.tar.gz";
-    sha256 = "0q909x456ihrnzlnv19bxv4fkjgr4syxsj6g06znks698hb78kg5";
+    url = "https://github.com/stcorp/muninn-ecmwfmars/archive/1.2.tar.gz";
+    sha256 = "15bp05hi9qqyyj69scg3lb36fm1q8lwyg0zvi3wskhx1dhcivr7z";
   };
 
   propagatedBuildInputs = [ coda muninn ];
