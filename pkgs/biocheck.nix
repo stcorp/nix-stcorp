059727bca14c60f55c44ef4c255de8703dad9921
Sander Niemeijer <sander.niemeijer@stcorp.nl>
2023-12-05 14:14:57 +0100

    Update to latest version

--- pkgs/biocheck.nix
+++ pkgs/biocheck.nix
@@ -4,11 +4,11 @@ with builtins;
 
 buildPythonPackage {
   pname = "biocheck";
-  version = "2023-12-01";
+  version = "2023-12-05";
 
   src = fetchGit {
     url = "https://github.nl/stcorp/biocheck.git";
-    rev = "a58d71c1b1834f17a2e65881ff749c6bc6566bee";
+    rev = "de82c36547f4af8bff6341d7bf1a85b96111daf0";
     ref = "main";
   };
 
