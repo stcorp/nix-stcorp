07edf7c4d618808500d1907ab6ce377d1d401555
Sander Niemeijer <sander.niemeijer@stcorp.nl>
2023-12-05 14:16:56 +0100

    Use latest HARP release

--- pkgs/harp.nix
+++ pkgs/harp.nix
@@ -16,12 +16,12 @@ let inherit (lib) optional optionals; in
 
 stdenv.mkDerivation {
   pname = "harp";
-  version = "1.20";
+  version = "1.20.2";
 
   src = fetchurl {
     # use official source package since we need the codadefs of the submodules to be included
-    url = "https://github.com/stcorp/harp/releases/download/1.20/harp-1.20.tar.gz";
-    sha256 = "0ax762wc68bgfjadp2wxhrmpz9y39x7r21ab99ggfai1cjq72mvb";
+    url = "https://github.com/stcorp/harp/releases/download/1.20.2/harp-1.20.2.tar.gz";
+    sha256 = "017beff923a0b0bcfadf99f04a046df40ceba58a82a3d9d55942f500cdc6a645";
   };
 
   buildInputs = [autoconf automake libtool bison flex zip hdf4 hdf5 coda]
