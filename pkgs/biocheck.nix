acc95df8f17f50b166821a88cbf385fe1dd89e82
Sander Niemeijer <sander.niemeijer@stcorp.nl>
2023-12-15 10:53:05 +0100

    Use 1.1 version

--- pkgs/biocheck.nix
+++ pkgs/biocheck.nix
@@ -4,11 +4,11 @@ with builtins;
 
 buildPythonPackage {
   pname = "biocheck";
-  version = "2023-12-13";
+  version = "2023-12-15";
 
   src = fetchGit {
     url = "https://github.com/stcorp/biocheck.git";
-    rev = "051342fbd9231de2fde0f69b3f32df77d41b0fe8";
+    rev = "583dbe42caef8d86a6daa9cdd22171393b8a63f9";
     ref = "main";
   };
 
