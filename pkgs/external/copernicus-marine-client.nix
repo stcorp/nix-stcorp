8a3bea5a116bd9a09ccdd42edd9787c51137e522
Sander Niemeijer <sander.niemeijer@stcorp.nl>
2024-01-17 10:44:00 +0000

    Relax dependencies to use NixOS 23.11 versions of packages

--- pkgs/external/copernicus-marine-client.nix
+++ pkgs/external/copernicus-marine-client.nix
@@ -6,7 +6,7 @@ let
   patch = writeText "copernicus-marine-client.patch" ''
     --- copernicus_marine_client-0.10.4-orig/pyproject.toml	2024-01-17 10:55:27
     +++ copernicus_marine_client-0.10.4/pyproject.toml	2024-01-17 10:56:27
-    @@ -14,7 +14,7 @@
+    @@ -14,14 +14,14 @@
      setuptools = "^68.2.2"
      motuclient = "^1.8.4"
      cachier = "^2.2.1"
@@ -15,6 +15,15 @@ let
      tqdm = "^4.65.0"
      zarr = "^2.13.3"
      pydap = "^3.2.2"
+     dask = ">=2022, <2024"
+    -netCDF4 = "^1.6.3"
+    +netCDF4 = "^1.6.2"
+     boto3 = ">=1.25,<1.34"
+    -semver = "^3.0.2"
+    +semver = "^3.0.1"
+     nest-asyncio = "^1.5.8"
+     pystac = "^1.8.3"
+     lxml = "^4.9.0"
   '';
 
 in buildPythonPackage rec {
