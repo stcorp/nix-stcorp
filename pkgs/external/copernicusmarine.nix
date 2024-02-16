c6288446329f6e7541ca575f6d37b781ec0c5741
Sander Niemeijer <sander.niemeijer@stcorp.nl>
2024-02-16 14:14:24 +0100

    Update to 1.0.2 of the copernicus marine client
    This release fixes the issue with creating cache directories, so our patch can be removed

--- pkgs/external/copernicusmarine.nix
+++ pkgs/external/copernicusmarine.nix
@@ -1,35 +1,15 @@
 { buildPythonPackage, fetchPypi, writeText, aiohttp, boto3, cachier, click, dask, lxml, motuclient, nest-asyncio, netcdf4, poetry-core, pydap, pystac, pythonRelaxDepsHook, requests, semver, setuptools, tqdm, xarray, zarr }:
 
-let
-
-  # Prevent the client from creating a cache directory when the package is imported
-  patch = writeText "copernicusmarine.patch" ''
-    --- copernicusmarine-1.0.0-orig/copernicusmarine/core_functions/utils.py	2024-01-23 16:55:27
-    +++ copernicusmarine-1.0.0/copernicusmarine/core_functions/utils.py	2024-01-29 08:53:19
-    @@ -30,9 +30,7 @@
-
-     CACHE_BASE_DIRECTORY = DEFAULT_CLIENT_BASE_DIRECTORY / "cache"
-
-    -pathlib.Path(CACHE_BASE_DIRECTORY).mkdir(parents=True, exist_ok=True)
-    -
-
-     DATETIME_SUPPORTED_FORMATS = [
-         "%Y",
-         "%Y-%m-%d",
-  '';
-
-in buildPythonPackage rec {
+buildPythonPackage rec {
   pname = "copernicusmarine";
-  version = "1.0.0";
+  version = "1.0.2";
   format = "pyproject";
 
   src = fetchPypi {
     inherit version;
     pname = "copernicusmarine";
-    sha256 = "e81b9a496b27a2df9c682f212047afcdcf9177a073f2c7461ad6d00858be42e9";
+    sha256 = "53ee976282c381455b758636a7e4f591ae9588a19fa493ed414e5646dafd766f";
   };
-
-  patches = [ patch ];
 
   pythonRelaxDeps = true;
 
