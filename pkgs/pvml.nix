a6fa541922fe82d9e2138160c9f07feb2db25ab1
Sander Niemeijer <sander.niemeijer@stcorp.nl>
2024-03-07 18:57:04 +0000

    Add libxml2 runtime dependency to pvml
    Apperently lxml does not have this explicit dependency itself

--- pkgs/pvml.nix
+++ pkgs/pvml.nix
@@ -1,4 +1,4 @@
-{ buildPythonPackage, lxml }:
+{ buildPythonPackage, libxml2, lxml }:
 
 with builtins;
 
@@ -11,5 +11,5 @@ buildPythonPackage {
     sha256 = "748e75c61de31f18e6358c138ad2e35d9b29124a82f6c77b9b463674612e6d00";
   };
 
-  propagatedBuildInputs = [ lxml ];
+  propagatedBuildInputs = [ libxml2 lxml ];
 }
