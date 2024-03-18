bfb45e2b523830330aeeaf17719aa0ba6d40fbaa
Sander Niemeijer <sander.niemeijer@stcorp.nl>
2024-03-18 16:39:35 +0100

    Update to latest release

--- pkgs/muninn-sentinel5p.nix
+++ pkgs/muninn-sentinel5p.nix
@@ -2,11 +2,11 @@
 
 buildPythonPackage {
   pname = "muninn-sentinel5p";
-  version = "2.2";
+  version = "3.0";
 
   src = builtins.fetchurl {
-    url = https://files.pythonhosted.org/packages/fa/b2/e98946cc82eb34751191f2b1b1e46e7b50d38f6153f16734155befc2f034/muninn-sentinel5p-2.2.tar.gz;
-    sha256 = "b30537118c8ec410dc668f98d8ef8222d3c57179fb4a0ca832896c0f33ccd36b";
+    url = "https://github.com/stcorp/muninn-sentinel5p/archive/3.0.tar.gz";
+    sha256 = "a2edfd1a07e6bac9c64b8ae0240369d65fc13e988ac0b5fe164614deb3df317a";
   };
 
   propagatedBuildInputs = [ coda muninn ];
