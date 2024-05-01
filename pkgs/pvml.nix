79f94e12ce5d952974c4d1b4a86c9a560eaec48e
Sander Niemeijer <sander.niemeijer@stcorp.nl>
2024-05-01 18:28:43 +0200

    Update pvml

--- pkgs/pvml.nix
+++ pkgs/pvml.nix
@@ -4,11 +4,11 @@ with builtins;
 
 buildPythonPackage {
   pname = "pvml";
-  version = "4.1";
+  version = "4.1.1";
 
   src = fetchurl {
-    url = https://github.com/stcorp/pvml/archive/refs/tags/4.1.tar.gz;
-    sha256 = "385c3537b69011fb1c01290858908226118fa4b8ba8cf44407600e55986b9c94";
+    url = https://github.com/stcorp/pvml/archive/refs/tags/4.1.1.tar.gz;
+    sha256 = "f8abf9c4630433c7c429a2374e8dab345719140b05b6981396abf014f90dd031";
   };
 
   propagatedBuildInputs = [ libxml2 lxml ];
