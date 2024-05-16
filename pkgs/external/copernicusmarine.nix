203a278c6cd3ac00facf8b778759bfafcf09221b
Sander Niemeijer <sander.niemeijer@stcorp.nl>
2024-05-16 14:50:49 +0200

    Update copernicusmarine toolbox

--- pkgs/external/copernicusmarine.nix
+++ pkgs/external/copernicusmarine.nix
@@ -2,13 +2,13 @@
 
 buildPythonPackage rec {
   pname = "copernicusmarine";
-  version = "1.0.2";
+  version = "1.2.1";
   format = "pyproject";
 
   src = fetchPypi {
     inherit version;
     pname = "copernicusmarine";
-    sha256 = "53ee976282c381455b758636a7e4f591ae9588a19fa493ed414e5646dafd766f";
+    sha256 = "e637347733b6e0610b63219448cddbd2e937d1612b8a1422c9523c49a41fd647";
   };
 
   pythonRelaxDeps = true;
