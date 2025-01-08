4bbba86ef17c685f7d487e7fd8352aeed8ce6a17
Sander Niemeijer <sander.niemeijer@stcorp.nl>
2025-01-08 14:02:18 +0100

    Use fetchPypi

--- pkgs/external/ecmwf-api-client.nix
+++ pkgs/external/ecmwf-api-client.nix
@@ -1,11 +1,11 @@
-{ buildPythonPackage, fetchurl }:
+{ buildPythonPackage, fetchPypi }:
 
 buildPythonPackage {
   pname = "ecmwf-api-client";
   version = "1.6.3";
 
-  src = fetchurl {
-    url = https://files.pythonhosted.org/packages/5b/44/ddeb18174b1cce4fe2c714c1968cd0e4272c6869c5073a3b270f071e5549/ecmwf-api-client-1.6.3.tar.gz;
+  src = fetchPypi {
+    inherit pname version;
     sha256 = "1mmhbnmbpcnjbwx1npnvxwrncamla13rlfcpihcxbqkj9aivs01s";
   };
 }
