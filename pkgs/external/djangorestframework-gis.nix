d061598d9d8537c86188720cb5664219b4f061d2
Sander Niemeijer <sander.niemeijer@stcorp.nl>
2025-01-08 14:04:11 +0100

    Update djangorestframework-gis

--- pkgs/external/djangorestframework-gis.nix
+++ pkgs/external/djangorestframework-gis.nix
@@ -1,14 +1,12 @@
-{ buildPythonPackage
-, djangorestframework
-}:
+{ buildPythonPackage, fetchPypi, djangorestframework }:
 
 buildPythonPackage {
   pname = "djangorestframework-gis";
-  version = "1.0";
+  version = "1.1";
 
-  src = builtins.fetchurl {
-    url = https://files.pythonhosted.org/packages/54/69/d099e8f8aa65d5158e9e0e34c7e0aefadfd98e62754d74dac55ecddba28a/djangorestframework-gis-1.0.tar.gz;
-    sha256 = "921c5adbc9a7c0502c905957a6695b67f55d7bf6582e1ab837888b55a1fce5a6";
+  src = fetchPypi {
+    inherit pname version;
+    sha256 = "84b915503a59263ed9473ecde34b19260c3e9c5c8ebb3aea8d91a67fd39f7215";
   };
 
   doCheck = false;
