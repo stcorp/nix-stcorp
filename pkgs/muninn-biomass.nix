99af6dfa8a53c29ec1cb4493072ee695e5fb0802
Sander Niemeijer <sander.niemeijer@stcorp.nl>
2025-04-19 18:59:05 +0200

    Update muninn-biomass

--- pkgs/muninn-biomass.nix
+++ pkgs/muninn-biomass.nix
@@ -4,11 +4,11 @@ with builtins;
 
 buildPythonPackage {
   pname = "muninn-biomass";
-  version = "2025-04-07";
+  version = "2025-04-19";
 
   src = fetchGit {
     url = "https://github.com/stcorp/muninn-biomass.git";
-    rev = "9290fb072b1cde08068aced68233ec7908847a1b";
+    rev = "fa5c06895a35582579f49909034698bdfafaaef4";
     ref = "main";
   };
 
