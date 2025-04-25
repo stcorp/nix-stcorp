775b83ec1e84047f44c9b4c554253020cf76e483
Sander Niemeijer <sander.niemeijer@stcorp.nl>
2025-04-25 11:16:26 +0200

    Update muninn-sentinel1

--- pkgs/muninn-sentinel1.nix
+++ pkgs/muninn-sentinel1.nix
@@ -4,11 +4,11 @@ with builtins;
 
 buildPythonPackage {
   pname = "muninn-sentinel1";
-  version = "2024-10-02";
+  version = "2025-04-25";
 
   src = fetchGit {
     url = "https://github.com/stcorp/muninn-sentinel1.git";
-    rev = "64782d8039126dfbe43886105b7c2d943bf93377";
+    rev = "57e61aa6b689549ccc0b5728c5ab8a77049dac3d";
     ref = "main";
   };
 
