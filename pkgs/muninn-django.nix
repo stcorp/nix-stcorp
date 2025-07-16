412100ffc71ca95075c658496e1f55ea6fe6e702
Sander Niemeijer <sander.niemeijer@stcorp.nl>
2025-07-16 18:09:51 +0200

    Update muninn-django

--- pkgs/muninn-django.nix
+++ pkgs/muninn-django.nix
@@ -1,12 +1,15 @@
 { buildPythonPackage, django, djangorestframework, djangorestframework-gis, muninn }:
 
+with builtins;
+
 buildPythonPackage {
   pname = "muninn-django";
-  version = "1.2.1";
+  version = "2025-07-16";
 
-  src = builtins.fetchurl {
-    url = "https://github.com/stcorp/muninn-django/archive/1.2.1.tar.gz";
-    sha256 = "36c76f4274ab6d5b2ffcc3ec87fb5a1a02439c5abd8609d5a7f5dde1416b0375";
+  src = fetchGit {
+    url = "https://github.com/stcorp/muninn-django.git";
+    rev = "38f63798a8f999b3b4b1cf68223fb5eb69fcea42";
+    ref = "master";
   };
 
   doCheck = false;
