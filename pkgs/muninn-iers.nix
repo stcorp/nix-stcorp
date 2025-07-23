e90b91ed7e71b84e2f7587794ff4ad43ac8fb3d8
Sander Niemeijer <sander.niemeijer@stcorp.nl>
2025-07-23 16:52:51 +0200

    Update muninn-iers

--- pkgs/muninn-iers.nix
+++ pkgs/muninn-iers.nix
@@ -4,11 +4,11 @@ with builtins;
 
 buildPythonPackage {
   pname = "muninn-iers";
-  version = "2025-07-22";
+  version = "2025-07-23";
 
   src = fetchGit {
     url = "https://github.com/stcorp/muninn-iers.git";
-    rev = "ec2e6a757079a1fea6bdb859c964f1723fd4d513";
+    rev = "08f9eb15d6e351ffb32cbd28fd644fee20fdeb0b";
     ref = "main";
   };
 
