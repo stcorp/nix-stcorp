ab52a33dbf3abf0e7d294bccf6097ddec4876829
Sander Niemeijer <sander.niemeijer@stcorp.nl>
2025-11-26 22:55:45 +0100

    Update muninn-earthcare

--- pkgs/muninn-earthcare.nix
+++ pkgs/muninn-earthcare.nix
@@ -7,11 +7,11 @@ with builtins;
 
 buildPythonPackage {
   pname = "muninn-earthcare";
-  version = "2025-10-07";
+  version = "2025-11-26";
 
   src = fetchGit {
     url = "https://github.com/stcorp/muninn-earthcare.git";
-    rev = "26361c3a9c0e4a9ee8ff32db1fd42a4975da24e5";
+    rev = "145533a8f5852a9099d631418503b6b90cac5958";
     ref = "main";
   };
 
