d8f6b6b7647b42b5fadf6d333ab546e2f849aa9f
Sander Niemeijer <sander.niemeijer@stcorp.nl>
2025-12-19 11:18:29 +0100

    Update muninn-eps-sg

--- pkgs/muninn-eps-sg.nix
+++ pkgs/muninn-eps-sg.nix
@@ -8,11 +8,11 @@ with builtins;
 
 buildPythonPackage {
   pname = "muninn-eps-sg";
-  version = "2025-08-04";
+  version = "2025-12-19";
 
   src = fetchGit {
     url = "https://github.com/stcorp/muninn-eps-sg.git";
-    rev = "0d1236ae7a5a567156cfc4018e73c6250fd9c08b";
+    rev = "6c564df845dd6d7f428989639ef0726103cfce2a";
     ref = "main";
   };
 
