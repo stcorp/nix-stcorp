39439f4e70f113dd57b2e7382224437a27ced932
Florian Huisman <florian.huisman@stcorp.nl>
2026-02-25 11:09:22 +0000

    Update to version 2.44.0

--- pkgs/external/python-eccodes.nix
+++ pkgs/external/python-eccodes.nix
@@ -10,7 +10,7 @@
 }:
 buildPythonPackage {
   pname = "python-eccodes";
-  version = "2.43.0";
+  version = "2.44.0";
   pyproject = true;
   build-system = [ setuptools ];
   propagatedBuildInputs = [
@@ -23,7 +23,7 @@ buildPythonPackage {
   src = fetchFromGitHub {
     owner = "ecmwf";
     repo = "eccodes-python";
-    rev = "2.43.0";
-    sha256 = "sha256-UN/hz3CWUGJW/z3nmm/tqmJFKCSX125Tv6rpsO+nDUs=";
+    rev = "2.44.0";
+    sha256 = "sha256-hDJWR1D5ZgVaq2Du12Z9T43Svz8Ze2yZms18hGJfS8k=";
   };
 }
