ccce97071fd357862340a7f9d49323a82e58c1ab
Sander Niemeijer <sander.niemeijer@stcorp.nl>
2026-06-01 16:01:21 +0200

    Update muninn-django

--- pkgs/muninn-django.nix
+++ pkgs/muninn-django.nix
@@ -9,11 +9,11 @@
 
 buildPythonPackage {
   pname = "muninn-django";
-  version = "1.3";
+  version = "1.3.1";
 
   src = builtins.fetchurl {
-    url = "https://github.com/stcorp/muninn-django/archive/1.3.tar.gz";
-    sha256 = "c7223843f8641059919bcba7b674b6bf1a180ff8903f69cd868eb5d53766c639";
+    url = "https://github.com/stcorp/muninn-django/archive/1.3.1.tar.gz";
+    sha256 = "071a03ec54e6826164825f103f72a584581be523aa21f43b8bee7c681567d3ea";
   };
 
   pyproject = true;
