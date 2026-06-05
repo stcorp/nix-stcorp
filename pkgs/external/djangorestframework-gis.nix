f9431cf4bc822b068b59af57dc8a9436ef6bed80
Sander Niemeijer <sander.niemeijer@stcorp.nl>
2026-06-05 14:02:49 +0200

    Update djangorestframework-gis

--- pkgs/external/djangorestframework-gis.nix
+++ pkgs/external/djangorestframework-gis.nix
@@ -8,12 +8,12 @@
 
 buildPythonPackage rec {
   pname = "djangorestframework-gis";
-  version = "1.2.0";
+  version = "1.2.1";
 
   src = fetchPypi {
     pname = "djangorestframework_gis";
     inherit version;
-    sha256 = "sha256-cCuprUQXO3zHDkjGyE2kjCj2+CYSzJAad/21TFxXyXE=";
+    sha256 = "649ad0cb361dd85f2113b1796248eb2b198a01fa3d64c5e486106224c0ad51fd";
   };
 
   pyproject = true;
