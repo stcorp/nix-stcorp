ea681f30d18bacfcc514aad520192e6ba759ed18
Leonard Huijzer <leonard.huijzer@stcorp.nl>
2026-08-12 14:36:52 +0200

    Update muninn-cams

--- pkgs/muninn-cams.nix
+++ pkgs/muninn-cams.nix
@@ -9,11 +9,11 @@ with builtins;
 
 buildPythonPackage {
   pname = "muninn-cams";
-  version = "2.3";
+  version = "2.3.1";
 
   src = fetchurl {
-    url = "https://github.com/stcorp/muninn-cams/archive/2.3.tar.gz";
-    sha256 = "4c918016fe4c68d36819f2b95adfed1fe8d4337779b9df66562e890b285ead1d";
+    url = "https://github.com/stcorp/muninn-cams/archive/2.3.1.tar.gz";
+    sha256 = "253fb9bef96b23db7da6d842ed7d6a29831dfb8f11a0a84daae8d76c0431fbe1";
   };
 
   pyproject = true;
